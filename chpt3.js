381bde46a03ea37b1966bcba52413b50a6941875
Rachel <regexina@gmail.com>
2016-10-07 23:36:12 -0400

    Fix isEven

--- chpt3.js
+++ chpt3.js
@@ -24,6 +24,21 @@ function isEven(number) {
         return isEven(number - 2)
 }
 
+// v.2
+function isEven(num) {
+    // Handles negative numbers
+    if (num < 0)
+        num *= -1
+    // Checks if number is even or odd
+    if (num % 2 === 0)
+        return true
+    else
+        return false
+}
+
+// v.2 is more efficent For example it returns test case num = -100 after only 6 steps
+// Recursive version after 256!!!
+
 // Solution 3 - Bean Counting
 // Given a string and character, returns how many times the character is in that string
 
